0d33789ff5c4c1127fed2684a212ffafdc5181c5
rororu <xylose.own@gmail.com>
2023-03-24 23:49:00 +0300

    Добавил enum с командами

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,24 @@
 package com.company;
 
+//--- Перечисление с командами ---
+enum Command {
+    //--- Команды ---
+    INC("/inc"), RESET("/reset"), STOP("/stop");
+
+    //--- Строковое значение команды ---
+    private final String commandName;
+
+    //--- Конструктор ---
+    Command(String commandName) {
+        this.commandName = commandName;
+    }
+
+    //--- Метод получения значения команды ---
+    String getCommandName() {
+        return commandName;
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
