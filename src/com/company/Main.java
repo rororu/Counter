7dce220307af3963b56c8b6b8132bdfb20d501c5
rororu <xylose.own@gmail.com>
2023-03-25 01:30:29 +0300

    Добавил финальные комментарии в реализацию

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -22,37 +22,49 @@ enum Command {
 
 public class Main {
 
+    //--- Точка входа в программу ---
     public static void main(String[] args) {
+        //--- Добавляю объекты программы ---
         Scanner input = new Scanner(System.in);
         Counter counter = new Counter();
         Info info = new Info();
         String command;
 
+        //--- Загружаю значение, вывожу его и начальную информацию для пользователя ---
         counter.loadValue();
         info.startInfo();
         info.valueInfo(counter.getValue());
 
+        //--- Бесконечный цикл ---
         for(;;) {
             try {
+                //--- Информирую о доступных командах и предлогаю ввести команду ---
                 info.commandInfo();
                 info.inputValueInfo();
 
+                //--- Ввод команды с консоли ---
                 command = input.nextLine();
 
+                //--- Сравниваю введенную команду с доступными ---
                 if (command.equals(Command.INC.getCommandName())) {
+                    //--- Увеличиваю значение на 1 и информирую пользователя ---
                     counter.updateValue();
                     info.newValueInfo(counter.getValue());
                 } else if (command.equals(Command.RESET.getCommandName())) {
+                    //--- Сбрасываю значение и информирую пользователя ---
                     counter.resetValue();
                     info.resetValueInfo(counter.getValue());
                 } else if (command.equals(Command.STOP.getCommandName())) {
+                    //--- Сохраняю значение и завершаю работу программы ---
                     counter.saveValue();
                     info.endAppInfo(counter.getValue());
                     break;
                 } else {
+                    //--- Выбрасываю исключение, если команда не верная ---
                     throw new WrongCommandException();
                 }
             } catch (WrongCommandException exception) {
+                //--- В блоке catch ловлю исключение, вывожу сведения об ощибке и инфорирую пользователя ---
                 System.out.println(exception.getMessage());
                 info.valueInfo(counter.getValue());
             }
